e920780536884c4a30012f1a670868a2d9c942b5
Adnan-shamsi <61737425+Adnan-shamsi@users.noreply.github.com>
2021-12-27 21:10:58 +0530

    Create SetMaxTimeForYouTube.js

---
+++ SetMaxTimeForYouTube.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.youtube.com*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    //alert('hi');
+     setTimeout(() => { window.location.replace("https://www.codeforces.com"); }, 10 * 60 * 1000); //after 5 min redirect
+    // Your code here...
+})();
