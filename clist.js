2ddf2f5943176f96b05eef8486785afb77cbf89d
Adnan-shamsi <61737425+Adnan-shamsi@users.noreply.github.com>
2022-02-02 11:04:20 +0530

    Create clist.js

---
+++ clist.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Showing only contest of certain platforms that you are interested in
+// @author       Adnan Shamsi
+// @match        https://clist.by/
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+window.addEventListener('load', (event) => {
+     'use strict';
+
+    const toRemoveList = ['.contest']
+
+    const toShowList = ['hackerearth.com',
+                        'codeforces.com',
+                        'codechef.com',
+                        'atcoder.jp',
+                        'codingcompetitions.withgoogle.com']                //add websites link that you are interested in
+
+    function execute( fn ){
+        try{fn()}catch(e){console.log(fn)}
+    }
+
+    function removeAll(...args){
+      args.forEach(arg  => document.querySelectorAll(arg).forEach(e => execute(() => e.style.display='none')))
+    }
+
+    function showOnlyDataOfCertainPlatform(){
+       toShowList.forEach(x =>{
+       console.log(x);
+       execute(()=>document.querySelectorAll(`a[data-original-title='${x}']`).forEach(e => e.closest('.contest').style.display = ''))
+       });
+    }
+
+    removeAll(...toRemoveList);
+    showOnlyDataOfCertainPlatform();
+})
